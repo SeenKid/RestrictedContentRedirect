1aea959bc8454dabe7af3adfba515df25b13a099
Yann@SeenKid <30565751+SeenKid@users.noreply.github.com>
2023-06-22 10:50:18 +0200

    Update RestrictedContentRedirect.php

--- RestrictedContentRedirect.php
+++ RestrictedContentRedirect.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: RestrictedContentRedirect
- * Author: Yann Berlemont
+ * Author: SeenKid
  * Version: 1.0
  * Description: Cette extension permet de faire des redirections customisées pour vos utilisateurs non connectés.
  * Plugin URI: https://github.com/SeenKid/RestrictedContentRedirect
@@ -24,5 +24,30 @@ function redirection()
       }
    }
 }
+
+add_action('template_redirect', 'redirection');
+
+// WOOCOMMERCE EXAMPLE 
+function redirection()
+{
+   // prends l'ID de la page que l'utilisateur essaie de charger
+   $pageid = get_queried_object_id();
+   $uri = $_SERVER['REQUEST_URI'];
+
+   // TODO : Update this.
+   $geonontisse = array("URL TO BLOCK");
+   // check si l'utilisateur est connecté ou non
+   if (!is_user_logged_in()) {
+
+      // Les pages bloquées pour les utilisateurs non connectés
+      foreach ($geonontisse as $check) {
+         if (strpos(strtolower($uri), strtolower($check)) !== false) {
+            // URL de redirection
+            wp_redirect('https://dev.mc2sarl.ch/geonontisse/');
+            exit();
+         }
+      }
+   }
+ }
 
 add_action('template_redirect', 'redirection');
